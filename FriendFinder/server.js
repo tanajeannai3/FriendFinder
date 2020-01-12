c574377989edd5e8b587b63b5fbb7e6151b9bd63
T'Ana Glasper <54415834+tanajeannai3@users.noreply.github.com>
2020-01-11 19:36:58 -0600

    Add files via upload

---
+++ FriendFinder/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var path = require('path');
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
